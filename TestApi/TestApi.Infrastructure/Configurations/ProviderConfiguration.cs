dbc901238dc76a0ab82dfc0f97eebac633957e90
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 12:58:12 +0300

    Added Provider Configuration

---
+++ TestApi/TestApi.Infrastructure/Configurations/ProviderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TestApi.Domain.Entities;
+
+namespace TestApi.Infrastructure.Configurations
+{
+    public class ProviderConfiguration : IEntityTypeConfiguration <Provider>
+    {
+        public void Configure(EntityTypeBuilder<Provider> builder)
+        {
+            builder.ToTable("Providers");
+            
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .IsRequired();
+        }
+    }
+}
