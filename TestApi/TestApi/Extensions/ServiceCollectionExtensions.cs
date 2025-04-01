ed8bba8e1c5ea3989e527a00ab20c68e79c9cd3b
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 14:50:41 +0300

    Registrated DbContext and Repository in App

---
+++ TestApi/TestApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApi.Domain.Interfaces.Repositories;
+using TestApi.Infrastructure;
+using TestApi.Infrastructure.Repositories;
+
+namespace TestApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Registration Of DbContext
+            services.AddDbContext<TestApiDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            //Registration of Repository
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            return services;
+        }
+    }
+}
--- TestApi/TestApi/Program.cs
+++ TestApi/TestApi/Program.cs
@@ -1,3 +1,5 @@
+using TestApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Calling method to registrate services
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
