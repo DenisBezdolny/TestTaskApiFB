eeb6272cda4cf84a6a44b6725903f2094a768dd8
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 21:13:35 +0300

    Added migration with correct relation

--- TestApi/TestApi.Infrastructure/Migrations/20250401180220_DeleteWrongRelationAndAddPresetProviderMigration.Designer.cs
+++ TestApi/TestApi.Infrastructure/Migrations/20250401180220_DeleteWrongRelationAndAddPresetProviderMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TestApi.Infrastructure;
@@ -11,9 +12,11 @@ using TestApi.Infrastructure;
 namespace TestApi.Infrastructure.Migrations
 {
     [DbContext(typeof(TestApiDbContext))]
-    partial class TestApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250401180220_DeleteWrongRelationAndAddPresetProviderMigration")]
+    partial class DeleteWrongRelationAndAddPresetProviderMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TestApi/TestApi.Infrastructure/Migrations/20250401180220_DeleteWrongRelationAndAddPresetProviderMigration.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TestApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteWrongRelationAndAddPresetProviderMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_Id",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Providers_ProviderId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ProviderId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "OrderItems",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.InsertData(
+                table: "Providers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Поставщик А" },
+                    { 2, "Поставщик B" },
+                    { 3, "Поставщик C" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DeleteData(
+                table: "Providers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Providers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Providers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "OrderItems",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ProviderId",
+                table: "Orders",
+                column: "ProviderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_Id",
+                table: "OrderItems",
+                column: "Id",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Providers_ProviderId",
+                table: "Orders",
+                column: "ProviderId",
+                principalTable: "Providers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
