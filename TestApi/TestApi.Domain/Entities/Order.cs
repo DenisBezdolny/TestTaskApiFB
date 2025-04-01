ab240e1073222245d4702f341d44172994196da3
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 13:05:19 +0300

    Added provider in order

--- TestApi/TestApi.Domain/Entities/Order.cs
+++ TestApi/TestApi.Domain/Entities/Order.cs
@@ -6,6 +6,8 @@
         public string Number { get; set; }
         public DateTime Date { get; set; }
         public int ProviderId { get; set; }
+
         public IEnumerable<OrderItem> OrderItems { get; set; }
+        public Provider Provider { get; set; }
     }
 }
