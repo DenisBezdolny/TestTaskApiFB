d133030002aac5e26885553725278ef66a520151
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 11:42:45 +0300

    Added repository interface

---
+++ TestApi/TestApi.Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApi.Domain.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync(); 
+    }
+}
