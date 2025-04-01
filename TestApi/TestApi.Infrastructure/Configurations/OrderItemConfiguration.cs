c65233d1d4237f5ab989531e215cdbc9ac5e73e6
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 12:43:23 +0300

    Created OrderItem Configuration

---
+++ TestApi/TestApi.Infrastructure/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TestApi.Domain.Entities;
+
+namespace TestApi.Infrastructure.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            builder.HasKey(oi => oi.Id);
+
+            builder.Property(oi => oi.Name)
+                .IsRequired();
+
+            builder.Property(oi => oi.Quantity)
+                .IsRequired()
+                .HasColumnType("decimal(18,3)");
+
+            builder.Property(oi => oi.Unit)
+                .IsRequired();
+        }
+    }
+}
