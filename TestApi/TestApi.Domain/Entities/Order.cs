83ef9ba0500b82de717e023d2c81ca9ed4d783c7
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-01 11:20:14 +0300

    Created Entities

---
+++ TestApi/TestApi.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public DateTime Date { get; set; }
+        public int ProviderId { get; set; }
+        public IEnumerable<OrderItem> OrderItems { get; set; }
+    }
+}
