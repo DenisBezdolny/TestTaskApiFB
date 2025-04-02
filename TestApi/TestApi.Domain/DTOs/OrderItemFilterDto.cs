d515b334ca6d75ac006ad60ac2de97326231d002
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-02 11:33:18 +0300

    Added Filter Dtos

---
+++ TestApi/TestApi.Domain/DTOs/OrderItemFilterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApi.Domain.DTOs
+{
+    public class OrderItemFilterDto
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public decimal? MinQuantity { get; set; }
+        public decimal? MaxQuantity { get; set; }
+    }
+}
