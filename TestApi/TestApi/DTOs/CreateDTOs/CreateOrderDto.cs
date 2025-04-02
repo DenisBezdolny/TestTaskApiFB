aa26b213b8d17175b4061ae5671af99218b0831b
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-03 01:08:22 +0300

    Added create DTOs

---
+++ TestApi/TestApi/DTOs/CreateDTOs/CreateOrderDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApi.DTOs.CreateDTOs
+{
+    namespace TestApi.DTOs.CreateDTOs
+    {
+        public class CreateOrderDto
+        {
+            public string Number { get; set; }
+            public DateTime Date { get; set; }
+            public int ProviderId { get; set; }
+            public List<CreateOrderItemDto> OrderItems { get; set; } = new List<CreateOrderItemDto>();
+        }
+    }
+
+}
