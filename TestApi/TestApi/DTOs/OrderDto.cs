5660f1d9125b5d5cbef3e3c8e0aee6721e29bdc4
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-03 01:08:53 +0300

    Added DTOs for update and return

---
+++ TestApi/TestApi/DTOs/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApi.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public DateTime Date { get; set; }
+        public int ProviderId { get; set; } 
+        public List<OrderItemDto> OrderItems { get; set; } = new List<OrderItemDto>();
+    }
+}
