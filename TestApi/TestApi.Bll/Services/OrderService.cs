51fd51401c8b05f95db634ada0f032be4bccc221
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-02 11:35:02 +0300

    Preliminary implementation of methods

--- TestApi/TestApi.Bll/Services/OrderService.cs
+++ TestApi/TestApi.Bll/Services/OrderService.cs
@@ -1,6 +1,96 @@
-﻿namespace TestApi.Bll.Services
+﻿using LinqKit;
+using System.Linq.Expressions;
+using TestApi.Domain.DTOs;
+using TestApi.Domain.Entities;
+using TestApi.Domain.Interfaces.Bll;
+using TestApi.Domain.Interfaces.Repositories;
+
+namespace TestApi.Bll.Services
 {
-    internal class OrderService
+    public class OrderService : IOrderService
     {
+        private readonly IRepository<Order> _orderRepository;
+
+        public OrderService(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersAsync(OrderFilterDto filter)
+        {
+            if (filter == null)
+            {
+                return await _orderRepository.GetAllAsync();
+            }
+
+            if (!filter.StartDate.HasValue)
+            {
+                filter.StartDate = DateTime.Today.AddMonths(-1);
+            }
+
+            // Start with always true predicete
+            var predicate = PredicateBuilder.New<Order>(true);
+
+            if (!string.IsNullOrEmpty(filter.Number))
+            {
+                predicate = predicate.And(o => o.Number.Contains(filter.Number));
+            }
+
+            if (filter.StartDate.HasValue && filter.EndDate.HasValue)
+            {
+                predicate = predicate.And(o => o.Date >= filter.StartDate.Value && o.Date <= filter.EndDate.Value);
+            }
+
+            if (filter.ProviderId.HasValue)
+            {
+                predicate = predicate.And(o => o.ProviderId == filter.ProviderId.Value);
+            }
+
+            Expression<Func<Order, bool>> filterExpression = (Expression<Func<Order, bool>>)predicate.Expand();
+            return await _orderRepository.GetAllAsync(filterExpression);
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int id)
+        {
+            return await _orderRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateOrderAsync(Order order)
+        {
+            
+            var existingOrders = await _orderRepository.GetAllAsync();
+            if (existingOrders.Any(o => o.Number == order.Number && o.ProviderId == order.ProviderId))
+            {
+                throw new InvalidOperationException("Заказ с таким номером уже существует для данного поставщика.");
+            }
+
+            if (order.OrderItems != null && order.OrderItems.Any(oi => oi.Name == order.Number))
+            {
+                throw new InvalidOperationException("Имя элемента заказа не может совпадать с номером заказа.");
+            }
+
+            await _orderRepository.CreateAsync(order);
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            var orders = await _orderRepository.GetAllAsync();
+            if (orders.Any(o => o.Id != order.Id && o.Number == order.Number && o.ProviderId == order.ProviderId))
+            {
+                throw new InvalidOperationException("Заказ с таким номером уже существует для данного поставщика.");
+            }
+
+            if (order.OrderItems != null && order.OrderItems.Any(oi => oi.Name == order.Number))
+            {
+                throw new InvalidOperationException("Имя элемента заказа не может совпадать с номером заказа.");
+            }
+
+            await _orderRepository.UpdateAsync(order);
+        }
+
+        public async Task DeleteOrderAsync(int id)
+        {
+            await _orderRepository.DeleteAsync(id);
+        }
     }
 }
--- TestApi/TestApi.Bll/Services/ProviderService.cs
+++ TestApi/TestApi.Bll/Services/ProviderService.cs
@@ -1,6 +1,26 @@
-﻿namespace TestApi.Bll.Services
+﻿using TestApi.Domain.Entities;
+using TestApi.Domain.Interfaces.Bll;
+using TestApi.Domain.Interfaces.Repositories;
+
+namespace TestApi.Bll.Services
 {
-    internal class ProviderService
+    public class ProviderService : IProviderService
     {
+        private readonly IRepository<Provider> _providerRepository;
+
+        public ProviderService(IRepository<Provider> providerRepository)
+        {
+            _providerRepository = providerRepository;
+        }
+
+        public async Task<IEnumerable<Provider>> GetProvidersAsync()
+        {
+            return await _providerRepository.GetAllAsync();
+        }
+
+        public async Task<Provider> GetProviderByIdAsync(int id)
+        {
+            return await _providerRepository.GetByIdAsync(id);
+        }
     }
 }
