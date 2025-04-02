3fbdaadd4bb10827878f08c3c7cbcce094e84bdc
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-03 01:11:12 +0300

    Added CORS

--- TestApi/TestApi/Program.cs
+++ TestApi/TestApi/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") 
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 //Calling method to registrate services
 builder.Services.AddApplicationServices(builder.Configuration);
 
@@ -20,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
